0675b042c2412a5427fa997f42e0782736f27c16
engineerscodes <practicalengineers05@gmail.com>
2020-11-22 15:50:47 +0530

    Initial Commit

---
+++ CrytoChat/src/main/java/login_page.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+import java.util.*;
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class login_page 
+{
+
+	public login_page() 
+	{
+		
+	}
+
+}
+
